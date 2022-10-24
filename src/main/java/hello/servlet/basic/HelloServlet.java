897de5506e721a978c0d9c3b0858d4d8a65f810d
gyunny <xorbs15923@naver.com>
2022-10-24 16:13:29 +0900

    Spring MVC - hello servlet

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,8 +11,20 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plan");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello" + username);
+
+
     }
 
 }
